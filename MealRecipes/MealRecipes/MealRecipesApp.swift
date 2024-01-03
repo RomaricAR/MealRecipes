dfcea37694782bb05c113a1ef64c4bd6fb88168d
RomaricAR <romaric@neuroleapmail.com>
2024-01-02 17:29:16 -0800

    Initial Commit

---
+++ MealRecipes/MealRecipes/MealRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealRecipesApp.swift
+//  MealRecipes
+//
+//  Created by Romaric Allahramadji on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MealRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
