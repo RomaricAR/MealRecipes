d4257a990b019fc0d1a98a1ca761524360fe6f41
RomaricAR <romaric@neuroleapmail.com>
2024-01-02 21:58:12 -0800

    Successfully fetched data from JSON and displayed  the content.

---
+++ MealRecipes/MealRecipes/Models/RecipeManager.swift
@@ -0,0 +1,8 @@
+//
+//  RecipeManager.swift
+//  MealRecipes
+//
+//  Created by Romaric Allahramadji on 1/2/24.
+//
+
+import Foundation
---
+++ MealRecipes/MealRecipes/ViewModel/FetchRecipesViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  MealRecipes
+//
+//  Created by Romaric Allahramadji on 1/2/24.
+//
+
+import Foundation
