2c13996a0cb0e2c4743ec6f577d9d8e8269391d6
RomaricAR <romaric@neuroleapmail.com>
2024-01-10 20:06:16 -0800

    Started implementing Unit Tests.

---
+++ MealRecipes/MealRecipesTests/RecipeViewModel_Tests.swift
@@ -0,0 +1,16 @@
+//
+//  MealRecipesTests.swift
+//  MealRecipesTests
+//
+//  Created by Romaric Allahramadji on 1/7/24.
+//
+
+import XCTest
+import MealRecipes
+
+final class MealRecipesTests: XCTestCase {
+    func testGetRecipes() {
+        let viewModel = MealRecipes.RecipeViewModel()
+    }
+
+}
